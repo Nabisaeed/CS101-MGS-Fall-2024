3abc14749da8fa6a384186c5aa6d1f977dbd9c80
Said Nabi <117791475+Nabisaeed@users.noreply.github.com>
2024-10-03 10:31:05 +0500

    Basics

---
+++ Basics/Hello_World.cpp
@@ -0,0 +1,13 @@
+// C++ Program starts here
+#include <iostream>
+using namespace std;
+    
+    
+int main()
+{
+
+    cout<<"Hello World"<<endl;
+    
+     return 0;
+    
+}
---
+++ Basics/Using_Namepsace.pp
@@ -0,0 +1,13 @@
+ // C++ Program starts here
+#include <iostream>
+    
+    
+int main()
+{
+int cout=10;
+   
+   std::cout<<cout<<std::endl;
+    
+     return 0;
+    
+}
